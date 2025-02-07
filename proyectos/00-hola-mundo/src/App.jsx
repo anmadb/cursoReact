f63742bdfbb6f505453ff5955b9815a20b001c18
juanm <juanmartindvd@gmail.com>
2025-02-07 11:04:44 +0100

    componentes seguir tw

--- proyectos/00-hola-mundo/src/App.jsx
+++ proyectos/00-hola-mundo/src/App.jsx
@@ -1,31 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import { TwitterFollowCard } from './assets/twFollowCard';
 
 export function App (){
-    const formattedUserName = (<span>@midudev</span>)
+    
+    const [name, setName] = useState('midudev');
+
+    console.log('name', name);  
 
     return (
         <section className='App'>
-        <TwitterFollowCard 
-        formattedUserName={formattedUserName} 
-        isFollowing 
-        userName="midudev" 
-        name= "Miguel Angel Durand" 
-        />
-        
-        <TwitterFollowCard 
-        formattedUserName={formattedUserName} 
-        isFollowing 
-        userName="zxvgxl" 
-        name= "Zavegol" 
-        />
+            <TwitterFollowCard userName={name}>
+                Miguel Angel Durand
+            </TwitterFollowCard>
 
-        <TwitterFollowCard  
-        formattedUserName={formattedUserName} 
-        isFollowing={true} 
-        userName="juanmadb_" 
-        name= "Juanma" 
-        />
+            <TwitterFollowCard userName="zxvgxl" >
+            zxvgxl
+            </TwitterFollowCard>
+
+            <TwitterFollowCard userName="juanmadb_">
+                Juanma
+            </TwitterFollowCard>
+
+            <button onClick={() =>setName('Juanma')}>
+                Cambio nombre
+            </button>
 
         </section>
     );
--- proyectos/00-hola-mundo/src/assets/twFollowCard.jsx
+++ proyectos/00-hola-mundo/src/assets/twFollowCard.jsx
@@ -1,24 +1,37 @@
-export function TwitterFollowCard ( {formattedUserName, userName, name, isFollowing }) {
 
-    console.log(isFollowing)
+import { useState } from "react" 
 
-    const imageSrc = `https://unavatar.io/${userName}`
+export function TwitterFollowCard ( {children, userName = 'unknow' }) {
 
-    return (
+
+    const [isFollowing, setIsFollowing] = useState(false)
+
+
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing 
+    ? 'tw-followcard-button is-following'
+    : 'tw-followcard-button'
+
+    const handleClick  = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    return ( 
         <article className="tw-followcard">
             <header className='tw-followcard-header'>
                 <img 
                 className='tw-followcard-avatar'
-                src={imageSrc} 
+                src={`https://unavatar.io/${userName}`} 
                 alt="El avatar de midudev" />
                 <div className='tw-followcard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followcard-infoUsername'>{formattedUserName}</span>
+                    <strong>{children}</strong>
+                    <span className='tw-followcard-infoUsername'>@{userName}</span>
                 </div>
             </header>
             <aside>
-                <button className='tw-followcard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
                 </button>
             </aside>
         </article>  
